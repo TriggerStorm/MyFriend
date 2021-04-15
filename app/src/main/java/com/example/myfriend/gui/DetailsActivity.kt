ebb8e434b64ed6dc2ce1f5d91266ea50ab9cba29
TriggerStorm <michael.h.p@hotmail.com>
2021-04-15 11:48:39 +0200

    pictur capture + preview works.

--- app/src/main/java/com/example/myfriend/gui/DetailsActivity.kt
+++ app/src/main/java/com/example/myfriend/gui/DetailsActivity.kt
@@ -1,9 +1,14 @@
 package com.example.myfriend.gui
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.res.Resources
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +18,7 @@ import com.example.myfriend.R
 
 class DetailsActivity : AppCompatActivity() {
     val isNew: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,7 @@ class DetailsActivity : AppCompatActivity() {
             val webside =findViewById<EditText>(R.id.ip_Website)
             val birthday = findViewById<EditText>(R.id.ip_Bday)
             val img = findViewById<ImageView>(R.id.img_pic)
+            //val savebn = findViewById<Button>()
             //val picture = findViewById<>(R.id.imgPicture)
             if (intent.extras != null) {
                 val extras: Bundle = intent.extras!!
@@ -34,6 +41,8 @@ class DetailsActivity : AppCompatActivity() {
                 val mDrawableName = friend.picture
                 val resID: Int = res.getIdentifier(mDrawableName, "drawable", packageName)
                // val drawable: Drawable = res.getDrawable(resID)
+
+                img.setOnClickListener { dispatchTakePictureIntent() }
 
 
                 img.setImageResource(R.drawable.trash)
@@ -51,4 +60,27 @@ class DetailsActivity : AppCompatActivity() {
 
         }
     }
+        val REQUEST_IMAGE_CAPTURE = 1
+
+         private fun dispatchTakePictureIntent() {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+            }
+
+
+        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras!!.get("data") as Bitmap
+            val img = findViewById<ImageView>(R.id.img_pic)
+            img.setImageBitmap(imageBitmap)
+        }
+
+    }
+
+
 }
