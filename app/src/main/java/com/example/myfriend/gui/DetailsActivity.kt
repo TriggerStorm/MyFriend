76f0fdf0e89808294c16c2b186755d6e24491fbd
TriggerStorm <michael.h.p@hotmail.com>
2021-04-16 20:27:48 +0200

    clean up delete added commeting

--- app/src/main/java/com/example/myfriend/gui/DetailsActivity.kt
+++ app/src/main/java/com/example/myfriend/gui/DetailsActivity.kt
@@ -47,14 +47,19 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        //this part is defining if is a new or existing friend
         var isNew = getIntent().getExtras()?.getBoolean("isNew");
         if (isNew != null) {
             areNew = isNew
         }
+        // remove log.
         Log.d("xyz", isNew.toString())
         Log.d("xyz", areNew.toString())
+
+        // if is a known friend it will run this code for edit the Friend profile
         if(isNew === false) {
 
+            // finding the view for reface from the Activity
            val tvName = findViewById<EditText>(R.id.ip_Name)
             val tvPhone = findViewById<EditText>(R.id.ip_phone)
             val address = findViewById<EditText>(R.id.ip_Address)
@@ -62,15 +67,18 @@ class DetailsActivity : AppCompatActivity() {
             val webside =findViewById<EditText>(R.id.ip_Website)
             val birthday = findViewById<EditText>(R.id.ip_Bday)
             val img = findViewById<ImageView>(R.id.img_pic)
+
+
+            /*
             val home = findViewById<Button>(R.id.bn_Home)
             val show = findViewById<Button>(R.id.bn_Show)
             val pcall = findViewById<ImageButton>(R.id.ibn_call)
             val pdel = findViewById<ImageButton>(R.id.ibn_delete)
             val ptext = findViewById<ImageButton>(R.id.ibn_text)
             val save = findViewById<Button>(R.id.bn_Save)
+            */
 
-
-
+            // if the extras is not null it will get the info about the Be_friend form the friend click in main activity
             if (intent.extras != null) {
                 val extras: Bundle = intent.extras!!
                 val friend = extras.getSerializable("friend") as BE_Friend
@@ -81,10 +89,9 @@ class DetailsActivity : AppCompatActivity() {
                 val resID: Int = res.getIdentifier(mDrawableName, "drawable", packageName)
 
 
+                //defining events and value in the view items with onclick and setting the value of the BE.
                 img.setOnClickListener { dispatchTakePictureIntent() }
-
                 PHONE_NO = friend.phoneNr
-
                 img.setImageResource(R.drawable.trash)
                 birthday.setText(friend.birthday)
                 webside.setText(friend.webside)
@@ -93,7 +100,7 @@ class DetailsActivity : AppCompatActivity() {
                 tvName.setText(friend.name)
                 tvPhone.setText(friend.phoneNr)
             } else {
-
+                // if is a new Friend hide un use / necessary buttons and img.
                 img.visibility = View.INVISIBLE
                 /*home.isVisible = false
                 show.visibility = View.INVISIBLE
@@ -110,10 +117,12 @@ class DetailsActivity : AppCompatActivity() {
 
         }
     }
+
         val REQUEST_IMAGE_CAPTURE = 1
 
     fun addNewFriend(view: View) {
 
+        // rough way of getting and setting defriend view elements and getting there value as string.   Note..... plz remind me what the import was to get direct access to the fx ip_Name form the xml THX!!
         val mRep = FriendRepositoryInDB.get()
         val tvName = findViewById<EditText>(R.id.ip_Name)
         val tvPhone = findViewById<EditText>(R.id.ip_phone)
@@ -131,7 +140,7 @@ class DetailsActivity : AppCompatActivity() {
         var imgText = ""
 
 
-
+        // adding new Friend to DB or else updating existing friend
         if(areNew == true){
         mRep.insert(BE_Friend(0,nametext,adressText, "",nrtext,mailtext,webText, bdText,""))
         }else
@@ -140,7 +149,9 @@ class DetailsActivity : AppCompatActivity() {
     }
 
          private fun dispatchTakePictureIntent() {
+             // seting the intent for the img capture
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+             // requesting to do img capture or catch eros
             try {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
             } catch (e: ActivityNotFoundException) {
@@ -149,6 +160,7 @@ class DetailsActivity : AppCompatActivity() {
 
 
         }
+    //if the request of the img capture and the result is oka then its mapping the data form the capture in a bit map and setting it as img. in view.
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
@@ -161,6 +173,7 @@ class DetailsActivity : AppCompatActivity() {
 
     lateinit var currentPhotoPath: String
 
+    // creating img file name and getting the external storage and creating the file for storage
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
@@ -177,16 +190,15 @@ class DetailsActivity : AppCompatActivity() {
     }
 
 
-    fun onClickSMS(view: View) {
-        showYesNoDialog()
-    }
 
+    // seting intent for useing call funktion on the phone and setting the number before executing the intent.
     fun onClickCALL(view: View) {
         val intent = Intent(Intent.ACTION_DIAL)
         intent.data = Uri.parse("tel:$PHONE_NO")
         startActivity(intent)
     }
 
+    //setting intent for useing email funktion on the phone and setting reciver before executing on the intent to sent mails.
     fun onClickEMAIL(view: View) {
         val emailIntent = Intent(Intent.ACTION_SEND)
         emailIntent.type = "plain/text"
@@ -195,7 +207,7 @@ class DetailsActivity : AppCompatActivity() {
         emailIntent.putExtra(Intent.EXTRA_EMAIL, receivers)
         startActivity(emailIntent)
     }
-
+    // seting intent for using phone web servie and setting url for webside berore calling the intent.
     fun onClickBROWSER(view: View) {
         val i = Intent(Intent.ACTION_VIEW)
         val webside = findViewById<EditText>(R.id.ip_Website)
@@ -203,17 +215,15 @@ class DetailsActivity : AppCompatActivity() {
         i.data = Uri.parse(url)
         startActivity(i)
     }
-
+    // seting intent for using phone sms app before setting reseaver and calling the intent for sms the intented nr.
     private fun startSMSActivity() {
         val sendIntent = Intent(Intent.ACTION_VIEW)
         sendIntent.data = Uri.parse("sms:$PHONE_NO")
         startActivity(sendIntent)
     }
 
-   //val PERMISSION_REQUEST_CODE = 1
 
-
-
+    // checking on Permission before calling funktion send message
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
@@ -223,11 +233,17 @@ class DetailsActivity : AppCompatActivity() {
         }
     }
 
+    // setting up smsmanger and prep defuld start text for sms app
     private fun sendMessage() {
         val m = SmsManager.getDefault()
         val text = ""
         m.sendTextMessage(PHONE_NO, null, text, null, null)
     }
+    // call function showYesNoDialog
+    fun onClickSMS(view: View) {
+        showYesNoDialog()
+    }
+    // setting up alert for the user before routing to the sms app
     private fun showYesNoDialog() {
         val alertDialogBuilder = AlertDialog.Builder(this)
         alertDialogBuilder.setTitle("SMS Handling")
@@ -241,12 +257,14 @@ class DetailsActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     fun onClickGetLocation(view: View) {
+        // call funktion to start lisen for gps cordinates
         startListening()
-
+        // setiing up location service and gps provider
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
         val slocaton = location.toString()
 
+        // if extras is not null finde be of selcted friend and add new location to db.
         if (intent.extras != null) {
             val extras: Bundle = intent.extras!!
             val friend = extras.getSerializable("friend") as BE_Friend
@@ -256,7 +274,7 @@ class DetailsActivity : AppCompatActivity() {
         friend.location = location.toString()
 
         val mRep = FriendRepositoryInDB.get()
-
+        // updating new loaction
        if (location != null) {
             mRep.update(friend)
         }
@@ -269,7 +287,7 @@ class DetailsActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     private fun startListening() {
-
+        // creating a new location listner if its null and start counting for new chance of location.
         if (myLocationListener == null)
             myLocationListener = object : LocationListener {
                 var count: Int = 0
@@ -283,7 +301,7 @@ class DetailsActivity : AppCompatActivity() {
 
                 }
             }
-
+        // getting location service and gps and updating the latest new location from mylocationlistener
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 0,
@@ -291,7 +309,7 @@ class DetailsActivity : AppCompatActivity() {
                 myLocationListener!!)
 
     }
-
+    // stops the loping lisen on the gps
     private fun stopListening() {
 
         if (myLocationListener == null) return
@@ -299,12 +317,26 @@ class DetailsActivity : AppCompatActivity() {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationManager.removeUpdates(myLocationListener!!)
     }
-
+    /* stops Listening on gps when close the activity */
     override fun onStop(){
         stopListening()
         super.onStop()
     }
+    // deleting selected Friend form the DB.
+    fun onClicKDelete(view: View) {
+        val mRep = FriendRepositoryInDB.get()
+        if (intent.extras != null) {
+            val extras: Bundle = intent.extras!!
+            val friend = extras.getSerializable("friend") as BE_Friend
 
+            mRep.delete(friend)
+            finish()
+        }
+            else
+        {
+            return
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/myfriend/gui/MainActivity.kt
+++ app/src/main/java/com/example/myfriend/gui/MainActivity.kt
@@ -21,39 +21,24 @@ import androidx.lifecycle.Observer
 
 
 class MainActivity : AppCompatActivity() {
-
+    // bug fixing val.
     val TAG = "xyz"
 
-    val PHONE_NO = "12345678"
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // initialize the db.
         FriendRepositoryInDB.initialize(this)
         setupDataObserver()
 
+        // calls insert test data for filling db.
         //insertTestData()
-       /* val friends = DBFriends()
-
-        val lV = findViewById<ListView>(R.id.lvFriends)
-
-        val friendNames = friends.getAllNames()
-
-        val ad: ListAdapter = ArrayAdapter(
-                this,
-                android.R.layout.simple_list_item_1, friendNames
-        )
-
-        lV.adapter = ad
-
-
-        lV.setOnItemClickListener { _, _, position, _ -> onListItemClick(position) }*/
-
 
     }
-
+    // put test date in the db
     private fun insertTestData() {
         val mRep = FriendRepositoryInDB.get()
         mRep.insert(BE_Friend(0,"Jonas","jonnystreed 21", "x1 y 20","25485652","Jonasgmail.com","jonweb.org", "02/05/1958","R.drawable.favorite"))
@@ -63,7 +48,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupDataObserver() {
+        // instans  repo
         val mRep = FriendRepositoryInDB.get()
+        //setting up observerble for BE_Friend and adding new adapter as an arrayadapter to fill the list view in the activety main
         val updateGUIObserver = Observer<List<BE_Friend>>{ friend ->
             val asStrings = friend.map { f -> "${f.id}, ${f.name}, ${f.address}, ${f.phoneNr}, ${f.picture}"}
             val adapter: ListAdapter = ArrayAdapter(
@@ -75,16 +62,16 @@ class MainActivity : AppCompatActivity() {
             lV.adapter = adapter
             Log.d(TAG, "UpdateGUI Observer notified")
         }
+        // setting up and observeble with the live data and make on click listner for friends on the Listview to hand be_friend to details.
         mRep.getAllLiveData().observe(this, updateGUIObserver)
-
         val lV = findViewById<ListView>(R.id.lvFriends)
         lV.onItemClickListener = AdapterView.OnItemClickListener {_,_,pos,_ -> onListItemClick(pos)}
     }
 
     fun onListItemClick( position: Int ) {
         // position is in the list!
-        // first get the name of the person clicked
-        //val name = Friends().getAll()[position].name
+        // first get the name of the friend clicked
+        // and sitting be_friend as extra to hand the details the be that was click
         val intent = Intent(this, DetailsActivity::class.java)
         val friend = DBFriends().getAll()[position]
         intent.putExtra("friend", friend)
@@ -106,7 +93,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         val id: Int = item.getItemId()
-
+        // setting the events on click on defriend menu items
         when (id) {
             R.id.action_settings -> {
                 Toast.makeText(this, "Action Settings selected...", Toast.LENGTH_LONG).show()
